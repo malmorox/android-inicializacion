abc3c228536c47ccfec071f84c115c6d0fd91155
malmorox <marcos.almorox@gmail.com>
2025-10-09 12:50:32 +0200

    modificando ejercicio comida animales

--- app/src/main/java/app/comida/ComidaActivity.kt
+++ app/src/main/java/app/comida/ComidaActivity.kt
@@ -1,6 +1,7 @@
 package app.comida
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +13,26 @@ class ComidaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_comida_animales)
-        
+
+        var perroCard = findViewById<androidx.cardview.widget.CardView>(R.id.perro)
+        var gatoCard = findViewById<androidx.cardview.widget.CardView>(R.id.gato)
+
+        val colorNormal = getColor(R.color.comidas_animales_cardview)
+        val colorSelected = getColor(R.color.comidas_animales_cardview_selected)
+
+        var selectedCard: androidx.cardview.widget.CardView? = null
+
+        fun seleccionarCarta(card: androidx.cardview.widget.CardView) {
+            selectedCard?.setCardBackgroundColor(colorNormal)
+            card.setCardBackgroundColor(colorSelected)
+            selectedCard = card
+        }
+
+        perroCard.setOnClickListener {
+            seleccionarCarta(perroCard)
+        }
+        gatoCard.setOnClickListener {
+            seleccionarCarta(gatoCard)
+        }
     }
 }
