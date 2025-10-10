7f35942132f6e7d5cb3d88b3e17e2b4bd78c2b0b
malmorox <marcos.almorox@gmail.com>
2025-10-10 13:09:15 +0200

    añadiendo nuevo activity para practicar con scrolls

---
+++ app/src/main/java/app/scroll/ScrollActivity.kt
@@ -0,0 +1,16 @@
+package app.scroll
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.android_inicializacion.R
+
+class ScrollActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_scroll)
+    }
+}
