d79d266b749e0714ca1fd07bc9c4fba3e2cc2cb8
malmorox <marcos.almorox@gmail.com>
2025-10-16 10:50:05 +0200

    añadiendo archivos para practicar de navbar en la parte inferior

---
+++ app/src/main/java/app/navbar/NavbarActivity.kt
@@ -0,0 +1,21 @@
+package app.navbar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.android_inicializacion.R
+
+class NavbarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_navbar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
