1db100c187d60c6216dbc356b8d7bc0d33197bbc
malmorox <marcos.almorox@gmail.com>
2025-10-21 13:10:12 +0200

    cambiando funcionalidad de animaciones entre fragmentos

--- app/src/main/java/app/navbar/NavbarActivity.kt
+++ app/src/main/java/app/navbar/NavbarActivity.kt
@@ -9,27 +9,47 @@ import com.example.android_inicializacion.databinding.ActivityNavbarBinding
 class NavbarActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityNavbarBinding
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNavbarBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(Home())
+        replaceFragment(Home(), 0)
 
         binding.nav.setOnItemSelectedListener { item ->
             when(item.itemId) {
-                R.id.home -> replaceFragment(Home())
-                R.id.settings -> replaceFragment(Settings())
-                R.id.profile -> replaceFragment(Profile())
+                R.id.home -> replaceFragment(Home(), 0)
+                R.id.settings -> replaceFragment(Settings(), 1)
+                R.id.profile -> replaceFragment(Profile(), 2)
             }
             true
         }
     }
 
-    private fun replaceFragment(fragment : Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.contenedor,fragment)
-        fragmentTransaction.commit()
+    private fun replaceFragment(fragment: Fragment, newIndex: Int) {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        if (newIndex > currentIndex) {
+            transaction.setCustomAnimations(
+                R.anim.slide_in_right,
+                R.anim.slide_out_left,
+                R.anim.slide_in_left,
+                R.anim.slide_out_right
+            )
+        } else if (newIndex < currentIndex) {
+            transaction.setCustomAnimations(
+                R.anim.slide_in_left,
+                R.anim.slide_out_right,
+                R.anim.slide_in_right,
+                R.anim.slide_out_left
+            )
+        }
+
+        transaction.replace(R.id.contenedor, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+        currentIndex = newIndex
     }
 }
